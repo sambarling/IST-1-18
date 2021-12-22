6dd4d2fc17b584461dc64a100da7560f375ca58e
IVarpenI <96059666+IVarpenI@users.noreply.github.com>
2021-12-22 21:43:46 +0300

    Geo-APi 0.2.2
    Добавлена поддержка юнит-тестов

--- Шиганов Руслан Яковлевич/main.py
+++ Шиганов Руслан Яковлевич/main.py
@@ -6,27 +6,37 @@ import sys
 
 app = FastAPI()
 
-#запрос на получения данных для карты населения
-@app.get("/info/neighbour/{x}/{y}")
-async def root(x, y):
-    try:
-        map = 0
-        number1 = x
-        number2 = y
-        with open("pars.csv", encoding='utf-8') as r_file:
-            # Создаем объект reader, указываем символ-разделитель ";"
-            file_reader = csv.reader(r_file, delimiter=";")
-            # Считывание данных из CSV файла
-            for row in file_reader:
-                #поиск по координатам
-                if number1 == row[1] and number2 == row[2]:
-                    map = (row[4])
-                if map == 0:
-                    map = "Wrong coords"    
-    except ValidationError as e:
-        return {"error": str(e)}
-    return {"population_map": map}
+
+class GeoAPI:
+    def __init__(self):
+        pass
+
+    #запрос на получения данных для карты населения
+    @app.get("/info/neighbour/{x}/{y}")
+    def root(self, x, y):
+       try:
+            map = 0
+            number1 = x
+            number2 = y
+            with open("pars.csv", encoding='utf-8') as r_file:
+              #Создаем объект reader, указываем символ-разделитель ";"
+              file_reader = csv.reader(r_file, delimiter=";")
+              #Считывание данных из CSV файла
+              for row in file_reader:
+                  #поиск по координатам
+                  if number1 == row[1] and number2 == row[2]:
+                     map = (row[4])
+              if map == 0:
+                   map = "Wrong coords"
+       except ValidationError as e:
+           return {"error": str(e)}
+       return map
 
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=5000, log_level="info")
+
+
+
+
+
